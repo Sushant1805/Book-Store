5cc38542b24a630be7cf23bca354565b9d4d674c
Sushant <202302040002@mitaoe.ac.in>
2025-04-26 15:48:38 +0530

    h

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -18,8 +18,10 @@ EOF
 command -v pm2 &> /dev/null || npm install -g pm2
 
 # Start app using PM2
-pm2 start "$APP_DIR/server.js" --name "book-store-app" --env production
-pm2 startup | bash || true
+pm2 start "$APP_DIR/index.js" --name "book-store-app" --env production
+
+# Configure PM2 to start on system boot
+pm2 startup systemd --user ec2-user | bash || true
 pm2 save || true
 
 echo "Application started successfully."
